aa53c5a8adbd0d14371e8ab495ac09247ece8596
Danil Tuzhik <danil-tuzhik@yandex.ru>
2021-10-12 06:35:19 +0700

    App

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import uuid from 'react-uuid';
 import { nanoid } from 'nanoid';
 
 import '../styles/App.css';
 
 function App() {
-  const [value1, setValue1] = useState('');
-  const [value2, setValue2] = useState('');
-  const [value3, setValue3] = useState('');
-  const [value4, setValue4] = useState('');
-  const [value5, setValue5] = useState('');
+  const [arr, setArr] = useState([1, 2, 3]);
+  const index = 1;
 
-  return <>
-    <input value={value1} onChange={event => setValue1(event.target.value)} /><br/>
-    <input value={value2} onChange={event => setValue2(event.target.value)} /><br/>
-    <input value={value3} onChange={event => setValue3(event.target.value)} /><br/>
-    <input value={value4} onChange={event => setValue4(event.target.value)} /><br/>
-    <input value={value5} onChange={event => setValue5(event.target.value)} /><br/>
-    <p>{(+value1 + +value2 + +value3 + +value4 + +value5) / 5}</p>
-  </>
+  const res = arr.map((el, index) => <p key={index}>{el}</p>);
+
+  function funcSort() {
+    const copy = Object.assign([], arr).reverse();
+    setArr(copy);
+  }
+
+  return (
+    <>
+      <button onClick={() => setArr([...arr, arr.length + 1])}>+</button>
+      <button onClick={() => setArr([...arr.slice(0, arr.length - 1)])}>-</button>
+      <button onClick={() => setArr([...arr.slice(0, index), '!', ...arr.slice(index + 1)])}>change</button>
+      <button onClick={() => funcSort()}>sort</button>
+      {res}
+    </>
+  );
 }
 
 export default App;
-
-/*Даны 5 инпутов. Сделайте так, чтобы при вводе чисел в наши инпуты в абзац выводилось среднее арифметическое введенных чисел.*/
